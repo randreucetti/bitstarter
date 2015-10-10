0f5019800da23fff9f0dca3cac5d12f37fb03f4a
randreucetti <ross.andreucetti@gmail.com>
2015-10-10 15:21:14 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
+var fs = require('fs')
 var app = express()
 
+var file = fs.readFileSync('index.html');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-    response.send('Hello World 2!'); 
+    response.send(file.toString()); 
 })
 
 app.listen(app.get('port'), function() {
